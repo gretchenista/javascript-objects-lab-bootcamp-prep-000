1d085bb09dcf45006897ea64f8cb8ece423f1c46
Cutina Woof <gretchenista@gmail.com>
2018-02-19 01:31:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+var recipes = new Object ({
+  prop : 1,
+  prop2 : 2
+});
+
+function updateObjectWithKeyAndValue (recipes, prop, prop2) {
+
+ return Object.assign({}, recipes, {});
+}
+
+updateObjectWithKeyAndValue(recipes, prop2, 2);
+ 
+
+function destructivelyUpdateObjectWithKeyAndValue (recipes, prop, prop2) {
+  recipes[prop] = 1;
+  recipes[prop2] = 2;
+}
+
+recipes;
+
+function deleteFromObjectByKey (recipes, prop) {
+    var obj = { prop: 1};
+    var newObj = Object.assign({}, obj);
+    delete newObj.prop[1];
+}
+
+newObj;
+
+
+function destructivelyDeleteFromObjectByKey (recipes, prop, prop2) {
+  delete recipes[prop];
+  return recipes;
+}
